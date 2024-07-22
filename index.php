341a15ec2446a2d7e7756e354e91cb2abf283a12
Sachin Chandra <154724735+Sachinchandra5@users.noreply.github.com>
2024-07-22 13:05:21 +0530

    first

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+	echo "HOMEPAGE";
+	echo "one";
+ ?>
---
+++ nothing.php
@@ -0,0 +1,23 @@
+<?php 
+	$out = 2;
+	echo $out;
+	echo "<br/>";
+	var_dump($out);
+	echo "<br/>";
+	$in =5;
+	echo $in;
+	echo "<br/>";
+	var_dump($in);
+	$a = 566778;
+	echo $a;
+	echo "<br/>";
+	var_dump($a);
+	$f = true;
+	echo $f;
+	echo "<br/>";
+	var_dump($f);
+	$k =null;
+	echo $k;
+	echo "<br/>";
+	var_dump($k)
+?>
